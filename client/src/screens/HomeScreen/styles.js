bf5a1b41478b0f99f477d43fad7cd1f4f4f77509
AGhafori <aghafori@hotmail.com>
2021-10-13 22:42:19 +0200

    pushing client

---
+++ client/src/screens/HomeScreen/styles.js
@@ -0,0 +1,26 @@
+import StyleSheet from 'react-native';
+
+const styles = StyleSheet.create({
+    map: {
+      marginTop: 10,
+      marginBottom: 10,
+      backgroundColor: "#4444",
+      height: 200,
+      borderColor: "#000",
+    },
+     input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      flexDirection: "row",
+      marginBottom: 10,
+    },
+    text: {
+      height: 40,
+      margin: 12,
+      
+      padding: 10,
+      flexDirection: "row",
+    }
+  });
